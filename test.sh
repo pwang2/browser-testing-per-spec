2de11ab1008f82d5ea8d857952ebc7e3081e18dd
Peng Wang <peng.wang@uptake.com>
2017-09-15 11:03:34 -0500

    use paste

--- test.sh
+++ test.sh
@@ -3,9 +3,6 @@
 set -euo pipefail
 rm -rf dist;
 
-list_input=$(find ./test -iname "*.spec.js")
-list_output=$(echo "$list_input" | xargs -n 1 basename | awk '{print "./dist/"$0}')
-
 hasimg=$(docker images | grep tape-run >/dev/null ; echo $?)
 if [[ $hasimg -eq 1 ]]; then
   docker build -t tape-run .
@@ -19,6 +16,9 @@ function processFile() {
 }
 
 export -f processFile
+
+list_input=$(find ./test -iname "*.spec.js")
+list_output=$(echo "$list_input" | xargs -n 1 basename | awk '{print "./dist/"$0}')
 
 paste <(echo "$list_input") <(echo "$list_output") |
 parallel -j4 --colsep '\t' processFile {1} {2}
